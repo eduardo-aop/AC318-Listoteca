78d8ddea45975c2518f352f72df1df0a8025a9d8
JonathanDP <jpereira@gec.inatel.br>
2018-10-01 22:34:07 -0300

    Added the Teacher class and the Update function fot students

--- software/back-end/app/routers.js
+++ software/back-end/app/routers.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var path    = require('path');
 var student = require('./student.js');
+var teacher = require('./teacher.js');
 
 //create router object
 var router = express.Router();
@@ -13,6 +14,12 @@ module.exports = router;
 router.route('/student/:id').get(student.getStudentById);
 router.route('/student').get(student.getAllStudents);
 router.route('/student').post(student.saveStudent);
+router.route('/student').post(student.updateStudent);
+
+router.route('/teacher/:id').get(teacher.getTeacherById);
+router.route('/teacher').get(teacher.getAllTeachers);
+router.route('/teacher').post(teacher.saveTeacher);
+router.route('/teacher').post(teacher.updateTeacher);
 
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../../front-end/index.html'));
